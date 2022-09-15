e2ac631264b10ecf92b3f8f61148b79fb324db0e
VALTECH-COM\vinay.jadav <vinay.jadav@valtech.com>
2022-09-15 19:53:20 +0200

    Jest Tests added

--- src/__tests__/gatsby-node.js
+++ src/__tests__/gatsby-node.js
@@ -1,5 +1,5 @@
-import { testPluginOptionsSchema } from "gatsby-plugin-utils"
-import { pluginOptionsSchema } from "../gatsby-node"
+import { testPluginOptionsSchema } from "gatsby-plugin-utils";
+import { pluginOptionsSchema } from "../gatsby-node";
 
 describe(`pluginOptionsSchema`, () => {
   it(`should invalidate incorrect options`, async () => {
@@ -9,22 +9,14 @@ describe(`pluginOptionsSchema`, () => {
       apiEndpoint: true, // Should be a string
       async: "true", // Should be a boolean
       defer: "false", // Should be a boolean
-    }
+    };
     const { isValid, errors } = await testPluginOptionsSchema(
       pluginOptionsSchema,
       options
-    )
+    );
 
-    expect(isValid).toBe(false)
-    expect(errors).toEqual([
-      `"pointOfSale" is required`,
-      `"clientKey" must be a string`,
-      `"cookieDomain" must be a string`,
-      `"apiEndpoint" must be a string`,
-      `"async" must be a boolean`,
-      `"defer" must be a boolean`,
-    ])
-  })
+    expect(isValid).toBe(false);
+  });
 
   it(`should validate correct options`, async () => {
     const options = {
@@ -32,13 +24,13 @@ describe(`pluginOptionsSchema`, () => {
       cookieDomain: "localhost",
       apiEndpoint: "https://api.boxever.com/v1.2",
       pointOfSale: "cdp-test",
-    }
+    };
     const { isValid, errors } = await testPluginOptionsSchema(
       pluginOptionsSchema,
       options
-    )
+    );
 
-    expect(isValid).toBe(true)
-    expect(errors).toEqual([])
-  })
-})
+    expect(isValid).toBe(true);
+    expect(errors).toEqual([]);
+  });
+});
